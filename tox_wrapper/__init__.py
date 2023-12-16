6e04bdc16387b19ef5a0c11ad8c01c797f8d1613
emdee@macaw.me <emdee@macaw.me>
2023-12-16 14:51:35 +0000

    rm tox_wrapper/*

--- tox_wrapper/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- mode: python; indent-tabs-mode: nil; py-indent-offset: 4; coding: utf-8 -*-
-# You need a libs directory beside this directory
-# and you need to link your libtoxcore.so and libtoxav.so
-# and libtoxencryptsave.so into ../libs/
-# Link all 3 to libtoxcore.so if you have only libtoxcore.so
